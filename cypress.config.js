d5485dbeb774e3daf9ab3c9c90208dc34ba8e2d0
rafo18 <rafaelbarreto10@hotmail.com>
2023-12-19 19:35:25 -0400

    Se agrego la dependencia mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+	reporter: 'cypress-mochawesome-reporter',
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
+			require('cypress-mochawesome-reporter/plugin')(on);
 		},
 		viewportHeight:1080,
 		viewportWidth:1920,
@@ -11,7 +13,7 @@ module.exports = defineConfig({
 	},
 	
 	retries: {
-		runMode:0,
+		runMode:3,
 		openMode:0
 	},
 	video:false,
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import 'cypress-plugin-api';
+import 'cypress-mochawesome-reporter/register';
 
 
 // Alternatively you can use CommonJS syntax:
